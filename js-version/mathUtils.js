914c7b1c27b512e1a959cf97da00e94fba58fd91
1253William <williamofosuparwar@gmail.com>
2025-06-03 17:29:52 +0000

    script added

---
+++ js-version/mathUtils.js
@@ -0,0 +1,15 @@
+// A pure JavaScript function for performing addition on two positive integers                                                                                                                                                     
+ function addNumbers(a, b) {
+  return a + b;
+}
+
+
+// A Pure JavaScript function for alerting a greeting to a given user
+function greetUser(user) {
+  return `Hello, ${user.name}! You are ${user.age} years old.`;
+}
+
+// Sample usage
+console.log(addNumbers(4, 6));
+console.log(greetUser({ name: "Amina", age: 25 }));
+
